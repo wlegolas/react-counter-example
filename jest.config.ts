59eefab4c8298bfb43d95129a9c1fe272125f107
Weslley Alves <wesshead@gmail.com>
2023-11-06 19:36:01 -0300

    Configure absolute path alias

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,13 @@
 import type { JestConfigWithTsJest } from "ts-jest";
+import { pathsToModuleNameMapper } from "ts-jest";
+
+import { compilerOptions } from "./tsconfig.json";
 
 const jestConfig: JestConfigWithTsJest = {
   clearMocks: true,
   roots: ["<rootDir>"],
   preset: 'ts-jest',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>' }),
   testEnvironment: 'node',
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
   transform: {
