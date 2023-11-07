53eba69194377866b0410c3306cea00f936ec8a9
Weslley Alves <wesshead@gmail.com>
2023-11-07 09:24:33 -0300

    Change CounterInfo component to apply the number styles just in the count information

--- src/features/counter/components/counter-info/CounterInfo.tsx
+++ src/features/counter/components/counter-info/CounterInfo.tsx
@@ -3,5 +3,9 @@ import { useCounter } from '~/features/counter/hooks/useCounter';
 export function CounterInfo() {
   const { count } = useCounter();
 
-  return <h2 className="counter__number">Count is: {count}</h2>;
+  return (
+    <h2>
+      Count is: <span className="counter__number">{count}</span>
+    </h2>
+  );
 }
